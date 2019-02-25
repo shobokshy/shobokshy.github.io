67d250803a1a9ad8889e9c9b684d70e9112f6d21
shobokshy <>
2019-02-25 22:08:19 +0000

    Improved project component layouts

--- components/project.tsx
+++ components/project.tsx
@@ -1,4 +1,5 @@
 /** @jsx jsx */ import { jsx } from '@emotion/core'
+import { GitHub } from 'react-feather';
 import { Document } from 'prismic-javascript/d.ts/documents';
 //@ts-ignore
 import { RichText, Date } from 'prismic-reactjs';
@@ -53,10 +54,21 @@ class Project extends React.Component<Props, State> {
 
                     <div css={{
                         marginTop: 'auto',
-                        color: 'white'
+                        color: 'white',
+                        display: 'flex',
+                        justifyContent: 'space-between'
                     }}>
-                        <h4 css={{marginBottom: 0}}>{RichText.asText(this.state.data.title)}</h4>
-                        <div css={{fontWeight: 200}}>{RichText.asText(this.state.data.subtitle)}</div>
+                        <div>
+                            <h4 css={{marginBottom: 0, marginTop: 0}}>{RichText.asText(this.state.data.title)}</h4>
+                            <div css={{fontWeight: 200}}>{RichText.asText(this.state.data.subtitle)}</div>
+                        </div>
+                        <div>
+                            {this.state.data.github_link.target && (
+                                <a target={this.state.data.github_link.target} href={this.state.data.github_link.url}>
+                                    <GitHub className="icon" />
+                                </a>
+                            )}
+                        </div>
                     </div>
 
                 </div>
